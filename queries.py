4b8a8e90285254852dcb5e3f68a88f0d4ac278c7
jasenc <jasen.c8@gmail.com>
2015-08-14 22:45:09 -0700

    feat: query 1 complete

--- queries.py
+++ queries.py
@@ -1,8 +1,13 @@
 # Imports and configuration
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from database_setup import Base, Restaurant, MenuItem
-engine = create_engine('sqlite:///restaurantmenu.db')
+from database_setup import Base, Puppy, Shelter
+engine = create_engine('sqlite:///puppy_shelter.db')
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+puppies = session.query(Puppy).order_by(Puppy.name).all()
+
+for puppy in puppies:
+    print puppy.name
