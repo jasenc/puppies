afc4680a67277e0ca56a32c4cc8a658be5c3dd7a
jasenc <jasen.c8@gmail.com>
2015-08-23 11:52:59 -0700

    refactor: Flask config

--- puppies/__init__.py
+++ puppies/__init__.py
@@ -1,6 +1,8 @@
 # Import Flask and initiate application.
 from flask import (Flask)
-app = Flask(__name__)
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('config')
+app.config.from_pyfile('config.py')
 
 # Import Views
 import puppies.views  # noqa
--- runserver.py
+++ runserver.py
@@ -1,9 +1,3 @@
 # Import application.
 from puppies import app
-# Create a secret_key for flash method to keep track of users.
-# Note: this is a silly development key.
-app.secret_key = 'super_secret_key'
-# Enabling debug mode tells Flask to reload the server on code changes.
-# AND Provides a debugger in the browser.
-app.debug = True
-app.run(host='0.0.0.0', port=5000)
+app.run(host="0.0.0.0", port=5000)
